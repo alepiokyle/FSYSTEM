4c56604c7681b7e49c7876b8d9dda2b719bac21a
GE RA LD <alipiokyle44@gmail.com>
2025-11-13 18:00:23 +0800

    update

--- app/View/Components/adminComponent.php
+++ app/View/Components/adminComponent.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class adminComponent extends Component
+class AdminComponent extends Component
 {
     /**
      * Create a new component instance.
